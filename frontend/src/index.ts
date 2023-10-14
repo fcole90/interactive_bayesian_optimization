c92fc25cef7bb496f2ad66b3ab7121b77eaec79d
fcole90 <fcole90@gmail.com>
2023-10-14 19:55:23 +0200

    Use tsc to produce js scripts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,7 +1,13 @@
 // Main entrypoint
+import { ui } from './legacy/libs/ui.js'
 
-export const main = () => {
-  console.log('Loading typescript')
+
+export const main = {
+  bootstrap: () => {
+    console.log('Bootstrapping app')
+    const app = new ui.Application()
+    app.exec()
+  },
 }
 
 
